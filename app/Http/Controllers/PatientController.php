54b128bc52b49fdfa595e5405a9a184761a0e754
mahnev_sa <mahnev_sa@irgups.ru>
2023-06-08 09:16:38 +0800

    #1 fix

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -14,35 +14,15 @@ class PatientController extends Controller
         $requestData = $request->json()->all();
         $keys = ['first_name', 'last_name', 'birthdate'];
         $data = array_intersect_key($requestData, array_flip($keys));
-        $birthdate = Carbon::parse($data['birthdate']);
-        $now = Carbon::now();
-
-        $data['age'] = $birthdate->diffInDays($now);
-        $data['age_type'] = 'день';
-
-        if ($data['age'] > 30) {
-            $data['age'] = $birthdate->diffInMonths($now);
-            $data['age_type'] = 'месяц';
-        }
-
-        if ($data['age'] > 12) {
-            $data['age'] = $birthdate->diffInYears($now);
-            $data['age_type'] = 'год';
-        }
 
         $patient = new Patient($data);
 
-        $patient_id = rand(1, 10000); // Выбираем случайный ID для пациента. На практике вам потребуется более надежный метод генерации уникальных ID.
+        $patient_id = rand(1, 10000);
 
         Cache::put('patient_' . $patient_id, $patient, 300);
 
-        // Получаем текущий список ID пациентов из кэша (или пустой массив, если список еще не существует)
         $patient_ids = Cache::get('patient_ids', []);
-
-        // Добавляем ID нового пациента в список
         $patient_ids[] = $patient_id;
-
-        // Сохраняем обновленный список ID пациентов в кэше
         Cache::put('patient_ids', $patient_ids, 300);
 
         return response()->json($patient, 201);
@@ -50,12 +30,10 @@ class PatientController extends Controller
 
     public function index()
     {
-        // Получаем список ID пациентов из кэша
         $patient_ids = Cache::get('patient_ids', []);
 
         $patients = [];
 
-        // Загружаем каждого пациента по ID из кэша
         foreach ($patient_ids as $id) {
             $patient = Cache::get('patient_' . $id);
             if ($patient) {
--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Patient extends Model
 {
@@ -11,5 +12,27 @@ class Patient extends Model
     public function getNameAttribute()
     {
         return $this->first_name . ' ' . $this->last_name;
+    }
+    public function setBirthdateAttribute($value)
+    {
+        $birthdate = Carbon::parse($value);
+        $now = Carbon::now();
+
+        $age = $birthdate->diffInDays($now);
+        $age_type = 'день';
+
+        if ($age > 30) {
+            $age = $birthdate->diffInMonths($now);
+            $age_type = 'месяц';
+        }
+
+        if ($age > 12) {
+            $age = $birthdate->diffInYears($now);
+            $age_type = 'год';
+        }
+
+        $this->attributes['birthdate'] = $value;
+        $this->attributes['age'] = $age;
+        $this->attributes['age_type'] = $age_type;
     }
 }
